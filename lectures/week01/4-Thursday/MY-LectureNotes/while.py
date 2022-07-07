1124f8d5f7fe219dd7033d9adcc636443137ec88
skroll13 <skroll912@gmail.com>
2022-07-07 09:28:48 -0700

    adding more homework exercises

--- lectures/week01/4-Thursday/MY-LectureNotes/while.py
+++ lectures/week01/4-Thursday/MY-LectureNotes/while.py
@@ -1,15 +1,37 @@
 
 #1. Create a program that will print from 1-10 using a while loop.
+# number = 1
+# while number <= 10:
+#     print(number)
+#     number += 1
 
 #2. Create a program that will print from 10-1 using a while loop.
 
+# number = 10
+# while number >= 10:
+#     print(number)
+#     number -= 1
+
 #3. Create a program that prompts the user to enter a word.  The program doesn't stop asking the user to enter a word until the user enters the word "stop"
 
+# user_answer = input("please enter a word or I will keep asking ... (forever) >> ")
+
+# while user_answer.lower() != "stop":
+#     print(user_answer)
+#     user_answer = input("please enter a word or I will keep asking ... (forever) >> ") 
+#     if user_answer == "stop":
+#         print("well done")
+
 #4a. Create a program that has a variable named username and another variabled named password with values of your choice.
+username = "YourName"
+password = "Password!"
 
 #4b. Prompt the user for a username and then a password.
+username = input("Enter your username >> ")
+password = input("Enter your password >> ")
 
 #4c. If the both match continue on with the program and give a welcome message.
+
 
 #4d. If not it prompts the user for the username and password until they get it correct.
 
