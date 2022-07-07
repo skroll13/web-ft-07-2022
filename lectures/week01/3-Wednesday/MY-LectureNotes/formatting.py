36a9c2cd45b2dd8c30875da6c20d8fe73f621bf0
skroll13 <skroll912@gmail.com>
2022-07-06 19:13:59 -0700

    homework upload

--- lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
+++ lectures/week01/3-Wednesday/MY-LectureNotes/formatting.py
@@ -2,8 +2,6 @@
 # Create a variable called first_name and set the value to your first name
 first_name = "Shannon"
 
-first_name = "Veronica"
-
 # create a variable called last_name and set the value to your last name
 
 last_name = "Kroll"
@@ -16,21 +14,28 @@ print(first_name)
 print(last_name)
 
 # Create a new varaiable called my_name and set the value to the concatenation of your first name and your last name 
-my_name = f{first_name} + " " + {last_name}
+my_name = f"{first_name} {last_name}"
+
 
 # print "My name is ???" out to the console.  Substitue the ??? with the my_name variable.
-print("My name is my_name")
+print("My name is", my_name)
 
 # create a variable called company_name and set the value to "DigitalCrafts"
+company_name = "DigitalCrafts"
+
+print(company_name)
 
 
 # Using the two variables you set earlier, generate a company email address that follows the pattern: "first name, period, last name @ company domain". Assign the result to a new variable called email and print the email to the console.
 # harry.potter@DigitalCrafts.com
-
+email = f"{first_name}.{last_name}@{company_name}.com"
+print(email)
 
 # Using the firstname and lastname variables, generate a username to a new variable and print it to the console. The username should follow this format: first name, underscore, lastname.
 # harry_potter
 
+user_name = f"{first_name}_{last_name}"
+print(user_name)
 
 # Using all the information you have created, print the data in a nice format. That is, generate a "Contact Card" style output that looks better than just printing values.
 
@@ -38,6 +43,11 @@ print("My name is my_name")
 # Email: harry.potter@DigitalCrafts.com
 # Username: harry_potter
 
+print(f"""
+Welcome to {company_name}, {first_name} {last_name}!
+Email: {email}
+Username: {user_name}
+""")
 
 
 
---
+++ lectures/week01/3-Wednesday/homework/homework_Wed.py
@@ -0,0 +1,186 @@
+# # Homework
+
+# ## Python 101
+
+# #### 1. Tip Calculator
+
+# Your task is to write a program that calculates how much of a tip to leave at a restaurant.
+
+# Prompt the user for two things:
+
+# - The total bill amount
+# - The level of service, which can be one of the following: good, fair, or bad
+
+# Calculate the tip amount and the total amount (bill amount + tip amount). The tip percentage based on the level of service is based on:
+
+# - good -> 20%
+# - fair -> 15%
+# - bad -> 10%
+
+# good_service = 0.2
+# fair_service = 0.15
+# bad_service = 0.1
+
+# total_bill = float(input('enter bill total >>'))
+# level_of_service = input('enter good, fair, or bad >>')
+
+# if level_of_service == "good":
+#     tip = total_bill*good_service
+# elif level_of_service == "fair":
+#     tip = total_bill*fair_service
+# else: 
+#     tip = total_bill*bad_service
+
+# print(f"Tip is ${tip}0")
+# print(f"Your total bill is ${total_bill+tip}0")
+
+
+            
+
+# Example session:
+
+# ```
+# $ python3 tip_calc.py
+# Total bill amount? 100
+# Level of service? good
+# Tip amount: $20.00
+# Total amount: $120.00
+# ```
+
+# ```
+# $ python3 tip_calc.py
+# Total bill amount? 48
+# Level of service? bad
+# Tip amount: $4.80
+# Total amount: $52.80
+# ```
+
+# Hints:
+
+# - Remember that you need to convert the input from the user input to floats instead of ints. Use the `float` function instead of the `int` function.
+# - To format a float number as a dollar value, use Python's formatting syntax: `"%.2f" % amount`
+
+# #### 2. Tip Calculator 2
+
+# Allow the ability to divide the check into a equal parts amount a number of people. The user will enter the number of people to be divided amongst. Example session:
+
+# ```
+# $ python3 tip_calc2.py
+# Total bill amount? 100
+# Level of service? good
+# Split how many ways? 5
+# Tip amount: $20.00
+# Total amount: $120.00
+# Amount per person: $24.00
+# ```
+
+# good_service = 0.2
+# fair_service = 0.15
+# bad_service = 0.1
+
+# total_bill = float(input('enter bill total >>'))
+# level_of_service = input('enter good, fair, or bad >>')
+
+# if level_of_service == "good":
+#     tip = total_bill*good_service
+# elif level_of_service == "fair":
+#     tip = total_bill*fair_service
+# else: 
+#     tip = total_bill*bad_service
+    
+# split_bill = int(input('Split the bill how many ways? >>'))
+
+# print(f"Tip is ${tip}0")
+# print(f"Your total bill is ${total_bill+tip}0")
+# print(f"The bill is split {split_bill} ways")
+# print(f"Total amount per person ${(total_bill+tip)/split_bill}0")
+
+
+# #### 3. How many coins?
+
+# Write a program that will prompt you for how many coins you want. Initially you have no coins. It will ask you if you want a coin? If you type "yes", it will give you one coin, and print out the current tally. If you type no, it will stop the program. Example run:
+
+# ```
+# $ python3 coins.py
+# You have 0 coins.
+# Do you want another? yes
+# You have 1 coins.
+# Do you want another? yes
+# You have 2 coins.
+# Do you want another? no
+# Bye
+# ```
+
+# print("You have 0 coins.")
+# coin = 0
+
+# another_coin = input('Do you want another coin? Yes or No  ')
+# while another_coin == 'Yes':
+#     coin += 1
+#     print(f"You have {coin}.")
+#     another_coin = input('Do you want another coin? Yes or No  ')
+# else:
+#     print('See you later, Aligator')
+
+
+# #### 4. Print a Box
+
+# Given a height and width, input by the user, print a box consisting of * characters as its border. Example session:
+
+# ```
+# $ python3 box.py
+# Width? 6
+# Height? 4
+# ******
+# *    *
+# *    *
+# ******
+# ```
+
+width = int(input('How many digits wide is the box?  '))
+height = int(input('How many digits tall is the box?  '))
+for y in range(height):
+    row_stars = "" 
+    for x in range(width):
+        if (x == 0 or y == 0 or y == height -1) or x == width - 1:
+            row_stars = row_stars + "*"
+        else:
+            row_stars = row_stars + " "
+    print(row_stars)
+
+
+
+# # #### 5. Print a Triangle
+
+# Print a triangle consisting of * characters like this:
+
+# ```
+#    *
+#   ***
+#  *****
+# *******
+# ```
+
+# #### 6. Multiplication Table
+
+# Print the multiplication table for numbers from 1 up to 10. Example output:
+
+# ```
+# $ python3 multiplication_table.py
+# 1 X 1 = 1
+# 1 X 2 = 2
+# 1 X 3 = 3
+# 1 X 4 = 4
+# ...
+# 5 X 5 = 25
+# 5 X 6 = 30
+# 5 X 7 = 35
+# ...
+# 10 X 8 = 80
+# 10 X 9 = 90
+# 10 X 10 = 100
+# ```
+
+# ## Submit Homework
+# - Create an issue
+# - Title issue: Python101_[YourName] i.e. Python101_VeronicaLino
