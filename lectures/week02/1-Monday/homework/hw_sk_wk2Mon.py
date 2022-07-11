87fc845dc2a7cda2bdc695900f10f49f7e88fcb2
skroll13 <skroll912@gmail.com>
2022-07-11 13:28:21 -0700

    homework uploads

---
+++ lectures/week02/1-Monday/homework/hw_sk_wk2Mon.py
@@ -0,0 +1,66 @@
+# # Homework
+# ## Functions
+
+# #### 1. Find the smallest number
+# Write a function `smallest` that accepts a List of numbers as an argument.
+# It should return the smallest number in the List.
+
+# num_list = [10, 2, 3, 40, 5, 1]
+# num_list2 = [99, 98, 97]
+
+# def smallest_num(list1):
+#     list2 = list1[0]
+#     for temp_num in list1:
+#         if temp_num < list2:
+#             list2 = temp_num
+#     print(list2)
+#     return(list2)
+# smallest_num(num_list2)
+   
+    
+# #### 2. Find the largest number
+# Write a function `largest` that accepts a List of numbers as an argument.
+# It should return the largest number in the List.
+
+# num_list3 = [77, 78, 79, 80, 81, -1]
+
+# def largest_num(list1):
+#     list2=list1[0]
+#     for num in list1:
+#         if num > list2:
+#             list2 = num
+#     print(list2)
+#     return(list2)
+# largest_num(num_list3)
+
+
+# #### 3. Find the shortest String
+# Write a function `shortest` that accepts a List of Strings as an argument.
+# It should return the shortest String in the List.
+
+# text1 = ["This", "is", "a", "string"]
+
+# def shortest_str(list1):
+#     text2=text1[0]
+#     for text in list1:
+#         if len(text) < len(text2):
+#             text2 = text
+#     print(text2)
+#     return(text2)
+# shortest_str(text1)
+
+# #### 4. Find the longest String
+# Write a function `longest` that accepts a List of Strings as an argument.
+# It should return the longest String in the List.
+
+# text3 = ["This", "is", "a", "string"]
+
+# def longest_str(list1):
+#     text2=list1[0]
+#     for text in list1:
+#         if len(text) > len(text2):
+#             text2 = text
+#     print(text2)
+#     return(text2)
+# longest_str(text3)
+
