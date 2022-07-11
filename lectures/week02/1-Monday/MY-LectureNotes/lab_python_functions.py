44d40f173223bf9693f9f4247d81cbc101215c3f
skroll13 <skroll912@gmail.com>
2022-07-11 10:04:51 -0700

    downloading today's homework

---
+++ lectures/week02/1-Monday/MY-LectureNotes/lab_python_functions.py
@@ -0,0 +1,135 @@
+
+# 1. Write a function called "greeting" that prints "hello world" to the console
+
+
+# 2. Execute (call/ run) the "greeting function"
+
+# 3. Reduce this code using functions
+
+
+# print("Day 1: Students in SRE class")
+# print("lecture: git 101")
+# print("Shu")
+# print("Thomas")
+# print("Gustavo")
+# print("Alim")
+# print("Day 2: Students in SRE class")
+# print("lecture: git 102")
+# print("Shu")
+# print("Thomas")
+# print("Gustavo")
+# print("Alim")
+# print("Day 3: Students in SRE class")
+# print("lecture: python 101")
+# print("Shu")
+# print("Thomas")
+# print("Gustavo")
+# print("Alim")
+
+
+# 5. Nested Functions
+
+# 6. Functions with parameters
+# Create a function that creates the following recommendation letter.
+# The Parameters for the functions should be the first and last name person you
+# are recommending
+
+# def rec_letter(first_name, last_name, rec_firstName, rec_lastName):
+#     letter = f"""
+#     {first_name} {last_name}
+#     1234 Park St
+#     Anytown, Pennsylvania 12345
+
+#     April 14, 2019
+
+#     ABC College Admission’s Board
+#     1234 Butler Ave
+#     Everywhere, CA 12345
+
+#     Dear ABC College Admission’s Board:
+
+#     My name is {first_name} {last_name}. I have served as a science teacher at Parktown High School for the past fifteen years and have had the privilege to serve as {rec_firstName} {rec_firstName}'s teacher for the past three. I have also been {rec_firstName}+'\’s 'advisor on the science academic team here at school. Due to his qualifications, I feel that rec_firstName would be an excellent addition to your school.
+
+#     While he has been a student here, {rec_firstName} has always challenged himself academically, taking all of the AP courses that our school has to offer. He has been captain of the academic team for the past two years, showing strong leadership qualities and organizational skills. His superior written and verbal skills have far surpassed any student of his age.
+
+#     {rec_firstName} would bring much to your school, both in and out of the classroom. If you have any questions regarding {rec_firstName}'s qualifications, please contact me at (123) 555-5555 or at {first_name}.{last_name}@email.com.
+
+#     Sincerely,
+
+
+#     {first_name} {last_name}
+#     Science Department Head
+#     Park Town High School
+#     """
+#     print(letter)
+
+# rec_letter("Shannon", "Kroll", "Mark", "Dalrympl")
+
+# 7. Order of parameters
+# In the last example, reverse the order of the arguments when the function is
+# called (switch first name and last name) and look at the results
+
+# def rec_letter(last_name, first_name, rec_lastName, rec_firstName):
+#     letter = f"""
+#     {first_name} {last_name}
+#     1234 Park St
+#     Anytown, Pennsylvania 12345
+
+#     April 14, 2019
+
+#     ABC College Admission’s Board
+#     1234 Butler Ave
+#     Everywhere, CA 12345
+
+#     Dear ABC College Admission’s Board:
+
+#     My name is {first_name} {last_name}. I have served as a science teacher at Parktown High School for the past fifteen years and have had the privilege to serve as {rec_firstName} {rec_firstName}'s teacher for the past three. I have also been {rec_firstName}+'\’s 'advisor on the science academic team here at school. Due to his qualifications, I feel that rec_firstName would be an excellent addition to your school.
+
+#     While he has been a student here, {rec_firstName} has always challenged himself academically, taking all of the AP courses that our school has to offer. He has been captain of the academic team for the past two years, showing strong leadership qualities and organizational skills. His superior written and verbal skills have far surpassed any student of his age.
+
+#     {rec_firstName} would bring much to your school, both in and out of the classroom. If you have any questions regarding {rec_firstName}'s qualifications, please contact me at (123) 555-5555 or at {first_name}.{last_name}@email.com.
+
+#     Sincerely,
+
+
+#     {first_name} {last_name}
+#     Science Department Head
+#     Park Town High School
+#     """
+#     print(letter)
+
+# rec_letter("Shannon", "Kroll", "Mark", "Dalrympl")
+
+
+# 8 Write a function that accepts a List of numbers as an argument.
+# Return a new List that includes the only the even numbers.
+
+# list_of_nums = [1, 2, 3, 4, 5, 6]
+# new_list = []
+
+
+# def 
+
+
+# 9 Args and Keyword Args
+# use the recommendation letter exercise and name your arguments
+
+
+# Return statements break the code block
+
+
+# Placement of Functions
+
+
+# Local Scope
+
+
+# Local Variables
+
+# Do parameters and local variables have the same scope?
+
+
+# Global Variables
+
+
+# Global Variables and Local Variables with the Same Names
