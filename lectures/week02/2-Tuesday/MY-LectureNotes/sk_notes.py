2f4b21049aef60c92c6b2ee9611276fef40fc104
skroll13 <skroll912@gmail.com>
2022-07-12 08:54:25 -0700

    homework download

---
+++ lectures/week02/2-Tuesday/MY-LectureNotes/sk_notes.py
@@ -0,0 +1,19 @@
+#dictionaries
+
+#dictionary = {}
+
+#dictionaryName = {key : value}
+
+contacts = {
+    "firstName" : "Shannon",
+    "phone" : "333-333-3333",
+    "city" : "Phoenix",
+    "zip" : "77201",
+    "isFriendly": True,
+    "myList" : [0, 1, 2]
+}
+result = contacts["isFriendly"] #this is how to reference the dictionary
+print(result)
+#can have a list as a value, not a key
+#can have a function as a value, not a key
+
