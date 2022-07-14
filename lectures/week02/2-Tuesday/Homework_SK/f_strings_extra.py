308469da60b2189cc83439f693b40cbdef74d803
skroll13 <skroll912@gmail.com>
2022-07-14 09:08:48 -0700

    f string practice uploads

--- lectures/week02/2-Tuesday/Homework_SK/f_strings_extra.py
+++ lectures/week02/2-Tuesday/Homework_SK/f_strings_extra.py
@@ -7,20 +7,28 @@
 
 #! 2. repeat the 1st exercise, but this time print the name in capital letters, and the number of letters in the name
 # result: "Hello, Veronica! Your name has 8 letters in it"
-name = input('please enter your name: >> ')
-print(upper.(f'hello, {name}!'))
+# name = input('please enter your name: >> ')
+# print(f'hello, {name.upper()}! your name has {len(name)} in it')
 
 #! 3.Please promt user for:name, favourite subject in school and fill in the blanks below:
-# print("____(name)____'s favorite subject in school is _____(subject)______.")
+# name = input('what is your name? >> ')
+# subject = input('what is your favorite school subject? >> ')
+# print(f"{name}'s favorite subject in school is {subject}.")
 
 
 #!4 Given the following four element list:['oranges', 1.3, 'lemons', 1.1]. Write an f-string that will display:
-# The weight of an orange is 1.3 and the weight of a lemon is 1.1
+# # The weight of an orange is 1.3 and the weight of a lemon is 1.1
+# list=['oranges', 1.3, 'lemons', 1.1]
+# print(f'The weight of an {list[0]} is {list[1]} and the weight of a {list[2]} is {list[3]}')
 
 
 #!5Given these 2 varaibles: title = 'Joshua', experience = 24.  Write an f-string that will display:
 # My name is Joshua and I have 34 years of experience'
+# name='Joshua'
+# age=24
+# print(f'My name is {name} and I have {age} years of experience')
 
 
 #! Create a variable called first_name and set the value to your first name. print your first name in console using a f-string
-
+first_name="Shannon"
+print(f'{first_name}')
