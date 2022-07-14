658c95cbaab1bdc4b95a9267eaf1031069e9d326
skroll13 <skroll912@gmail.com>
2022-07-13 18:46:19 -0700

    extra practice upload

---
+++ lectures/week02/2-Tuesday/Homework_SK/f_strings_extra.py
@@ -0,0 +1,26 @@
+
+#! 1.create a variable 'name' from user input 
+#print a message that greets the user with his name: "Hello, Veronica!"
+# from unicodedata import name
+# name = input('please enter your name: >> ')
+# print(f'Hello, {name}!')
+
+#! 2. repeat the 1st exercise, but this time print the name in capital letters, and the number of letters in the name
+# result: "Hello, Veronica! Your name has 8 letters in it"
+name = input('please enter your name: >> ')
+print(upper.(f'hello, {name}!'))
+
+#! 3.Please promt user for:name, favourite subject in school and fill in the blanks below:
+# print("____(name)____'s favorite subject in school is _____(subject)______.")
+
+
+#!4 Given the following four element list:['oranges', 1.3, 'lemons', 1.1]. Write an f-string that will display:
+# The weight of an orange is 1.3 and the weight of a lemon is 1.1
+
+
+#!5Given these 2 varaibles: title = 'Joshua', experience = 24.  Write an f-string that will display:
+# My name is Joshua and I have 34 years of experience'
+
+
+#! Create a variable called first_name and set the value to your first name. print your first name in console using a f-string
+
