7ffb6f29f788261be6a872882822c005096d10a3
skroll13 <skroll912@gmail.com>
2022-07-22 09:48:11 -0700

    lab uploads

--- lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
+++ lectures/week03/5-Friday/MY-LectureNotes/dailyProblems/problems.py
@@ -1,6 +1,16 @@
 # Return a new list with each element multiplied by 5
 
-let nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
+from unicodedata import name
+
+
+nums = [56, 34, 34, 1, 1, 1, 23, 12, 89, 89, 89, 70, 56] 
+newList=[]
+for i in nums:
+    newList.append(i*5)
+print(newList)
 
 
 # Given a list [("name", "Elie"), ("job", "Instructor")], create a dictionary that looks like this {'job': 'Instructor', 'name': 'Elie'} (the order does not matter).
+myList=
+my_dictionary={}
+for in range(len(myList))
